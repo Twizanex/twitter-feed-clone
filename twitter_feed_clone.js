e61cc1e67caa5fd0e6268c2e42770049abfd15e7
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-04 19:14:17 +0100

    Almost finished impersonating the Twitter.com topbar

--- twitter_feed_clone.js
+++ twitter_feed_clone.js
@@ -6,7 +6,17 @@ function prepareNavHandler() {
   });
 }
 
+function prepareSettingsHandler() {
+  $("#settings a").hover(function() {
+    $(this).find('.settings-icon').addClass('hover-icon');
+    $(this).find('.settings-caret').addClass('hover-caret');    
+  }, function() {
+    $(this).find('.settings-icon').removeClass('hover-icon');
+    $(this).find('.settings-caret').removeClass('hover-caret');    
+  });
+}
+
 $(function() {
   prepareNavHandler();
-  prepareSearchHandler();
+  prepareSettingsHandler();
 });
